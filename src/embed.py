e6bc08067be9899b07742e7b659c505ccdada1ca
Manan Bhargava <manan.bhargava@berkeley.edu>
2023-10-28 19:11:25 -0700

    added starter code

---
+++ src/embed.py
@@ -0,0 +1,54 @@
+import os
+from dotenv import load_dotenv
+from supabase.client import Client, create_client
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import SupabaseVectorStore
+from langchain.document_loaders import TextLoader
+from langchain.document_loaders import TextLoader
+
+load_dotenv()
+
+supabase_url = os.environ.get("SUPABASE_URL")
+supabase_key = os.environ.get("SUPABASE_SERVICE_KEY")
+supabase: Client = create_client(supabase_url, supabase_key)
+
+# configure these to fit your needs
+# we dont care about other data types
+exclude_dir = ['.git', 'node_modules', 'public', 'assets']
+exclude_files = ['package-lock.json', '.DS_Store']
+exclude_extensions = ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff', '.ico', '.svg', '.webp',
+    '.mp3', '.wav']
+
+documents = []
+
+for dirpath, dirnames, filenames in os.walk('repo'):
+    # skip directories in exclude_dir
+    dirnames[:] = [d for d in dirnames if d not in exclude_dir]
+    
+    for file in filenames:
+        _, file_extension = os.path.splitext(file)
+
+        # skip files in exclude_files
+        if file not in exclude_files and file_extension not in exclude_extensions:
+            file_path = os.path.join(dirpath, file)
+            loader = TextLoader(file_path, encoding='ISO-8859-1')
+            documents.extend(loader.load())
+
+
+text_splitter = CharacterTextSplitter(chunk_size=2000, chunk_overlap=200)
+docs = text_splitter.split_documents(documents)
+
+for doc in docs:
+    source = doc.metadata['source']
+    cleaned_source = '/'.join(source.split('/')[1:])
+    doc.page_content = "FILE NAME: " + cleaned_source + "\n###\n" + doc.page_content.replace('\u0000', '')
+
+embeddings = OpenAIEmbeddings()
+
+vector_store = SupabaseVectorStore.from_documents(
+    docs,
+    embeddings,
+    client=supabase,
+    table_name=os.environ.get("TABLE_NAME"),
+)
